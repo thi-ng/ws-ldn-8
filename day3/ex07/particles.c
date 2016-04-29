616334593c274790c1221e79b7183439a4cf4ea4
Karsten Schmidt <k@postspectacular.com>
2016-04-29 15:18:59 +0100

    bugfix ex07/particles.c

--- day3/ex07/particles.c
+++ day3/ex07/particles.c
@@ -1,9 +1,11 @@
 // emcc -O2 -o index.html -s ASM_JS=1 -s NO_FILESYSTEM=1 --closure 1 particles.c
+// emcc -s ASM_JS=1 particles.c -o index.html
 // emcc -O2 -o index.html -s NO_FILESYSTEM=1 -s INVOKE_RUN=1 -s "EXPORT_NAME='Foo'" -s MODULARIZE=1 --closure 1 particles.c
 
 // f=new Float32Array(Module.HEAPF32.buffer, Module.HEAP32[5247920>>2], 384)
 // Module.ccall("dump",null,['ptr'],[5247920])
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <emscripten.h>
@@ -27,14 +29,14 @@ typedef struct {
 } ParticleSystem;
 
 
-Vec3* setVec3(Vec3 &v, float x, float y, float z) {
+Vec3* setVec3(Vec3 *v, float x, float y, float z) {
   v->x = x;
   v->y = y;
   v->z = z;
   return v;
 }
 
-Vec3* addVec3(Vec3 &v, Vec3 &v2) {
+Vec3* addVec3(Vec3* v, Vec3* v2) {
   v->x += v2->x;
   v->y += v2->y;
   v->z += v2->z;
@@ -43,24 +45,26 @@ Vec3* addVec3(Vec3 &v, Vec3 &v2) {
 
 ParticleSystem* makeParticleSystem(uint32_t num) {
   ParticleSystem *psys = (ParticleSystem*)malloc(sizeof(ParticleSystem));
-  psys->verts = (Particle*)malloc(num * sizeof(Particle));
+  psys->particles = (Particle*)malloc(num * sizeof(Particle));
   psys->numParticles = num;
   return psys;
 }
 
-ParticlesSystem* updateParticleSystem(ParticlesSystem *psys) {
+EMSCRIPTEN_KEEPALIVE ParticleSystem* updateParticleSystem(ParticleSystem* psys) {
   for(uint32_t i=0; i < psys->numParticles; i++) {
-    Particle *p = psys->particles[i];
+    Particle *p = &psys->particles[i];
     addVec3(&(p->pos), &(p->vel));
     addVec3(&(p->pos), &(psys->gravity));
   }
+  return psys;
 }
 
-EMSCRIPTEN_KEEPALIVE float dump(ParticleSystem *psys) {
-  return (psys->particles[0]).y;
+EMSCRIPTEN_KEEPALIVE float dump(ParticleSystem* psys) {
+  return (psys->particles[0]).pos.y;
 }
 
-int main() {
+EMSCRIPTEN_KEEPALIVE int main(int argc, char** argv) {
+  printf("Hello Emscripten!");
   ParticleSystem *psys = makeParticleSystem(10);
   setVec3(&(psys->emitPos), 0.f, 0.f, 0.f);
   setVec3(&(psys->emitDir), 0.f, 1.f, 0.f);
